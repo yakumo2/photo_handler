2aaf3d283864d3716cb18597eb1f9a46968f15ed
yakumo2 <monkhead@126.com>
2024-03-29 13:15:07 +0800

    v1.0

--- photo_handler.py
+++ photo_handler.py
@@ -6,7 +6,7 @@ from pillow_heif import register_heif_opener
 register_heif_opener()
 
 # 压缩图片并保存
-def compress_image(input_path, output_path):
+def compress_image(input_path, output_path, quality=60, max_size=400):
     try:
         # 如果输出文件已存在，则跳过压缩
         if os.path.exists(output_path):
@@ -19,21 +19,26 @@ def compress_image(input_path, output_path):
         if image.format == 'HEIC':
             output_path = output_path.replace('.jpeg', '.png')  # 修改输出路径为 PNG 格式
             image = image.convert("RGB")
+
+        # 如果是 PNG 格式，则转换为 JPEG 格式
+        if image.format == 'PNG':
+            output_path = output_path.replace('.jpeg', '.png')  # 修改输出路径为 PNG 格式
+            image = image.convert("RGB")
         
         # 获取原始图片尺寸
         width, height = image.size
         
         # 计算调整后的尺寸
         if width >= height:
-            new_width = min(width, 400)
+            new_width = min(width, max_size)
             new_height = int(height * (new_width / width))
         else:
-            new_height = min(height, 400)
+            new_height = min(height, max_size)
             new_width = int(width * (new_height / height))
         
         # 调整尺寸并保存
         resized_image = image.resize((new_width, new_height), Image.LANCZOS)
-        resized_image.save(output_path, quality=60)  # 设置压缩质量
+        resized_image.save(output_path, quality=quality)  # 设置压缩质量
         
         return True
     except Exception as e:
@@ -41,7 +46,10 @@ def compress_image(input_path, output_path):
         return False
 
 # 遍历文件夹中的图片并压缩保存
-def compress_images_in_folder(input_folder, output_folder):
+def compress_images_in_folder(input_folder, output_folder, high_quality=False):
+    max_size = 800 if high_quality else 400
+    max_quality = 100 if high_quality else 60
+    
     for root, dirs, files in os.walk(input_folder):
         # 生成对应的输出文件夹结构
         rel_path = os.path.relpath(root, input_folder)
@@ -52,11 +60,14 @@ def compress_images_in_folder(input_folder, output_folder):
         for file in tqdm(files, desc=f'Compressing images in {root}'):
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.heic')):
                 input_path = os.path.join(root, file)
-                output_path = os.path.join(output_root, file.split('.')[0] + '-small.jpeg')  # 添加 -small 后缀
-                compress_image(input_path, output_path)
+                output_path = os.path.join(output_root, file.split('.')[0] + '.jpeg')  # 添加 -small 后缀
+                compress_image(input_path, output_path, quality=max_quality, max_size=max_size)
 
 # 主函数
 if __name__ == "__main__":
-    input_folder = 'photo'
-    output_folder = 'compressed'
+    input_folder = '/Volumes/共享文件/photo'
+    output_folder = 'photo/photo_small'
+    output_folder_high_quality = 'photo/photo_big'
+    
     compress_images_in_folder(input_folder, output_folder)
+    compress_images_in_folder(input_folder, output_folder_high_quality, high_quality=True)
